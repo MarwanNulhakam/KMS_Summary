ec1161521d6fbb33ea7f56151dced950b494d852
MarwanNulhakam <wind_raider_zero@yahoo.com>
2016-05-25 17:08:12 +0800

    MN | Add new field for second last visit and second last weight

--- src/mainPackage/mainClass.java
+++ src/mainPackage/mainClass.java
@@ -12,25 +12,25 @@ package mainPackage;
 public class mainClass {
     public static void main(String[]args){
         data.Person babyMarwan = new data.Person("marwan",    true,           8,      
-//                                                   name, is male,  age(month),     
+//                                                   name,is male?,  age(month),     
                 
-                                                       5.7,         6.2, "2016/03/10");
+                                                       5.7,         6.2, "2016/03/10", 5.5, "2016/02/12");
 //                                          current weight, prev weight, last visit  
 
         data.Person babyZay = new data.Person("zay",    true,           6,      
 //                                             name, is male,   age(month),     
                 
-                                                       6.2,         5.7, "2016/03/10");
+                                                       6.2,         5.7, "2016/03/10", 4.8, "2016/02/01");
 //                                          current weight, prev weight, last visit 
 
         data.Person babyRana = new data.Person("rana",    false,           4,      
 //                                               name,  is male,  age(month),     
                 
-                                                       6.0,         0, "2016/01/10");
+                                                       6.0,         0, "2016/01/10", 4.4, "2015/12/08");
 //                                          current weight, prev weight, last visit  
 
         
-        calculation.Calculator machine = new calculation.Calculator();
+        calculation.KMSCalculator machine = new calculation.KMSCalculator();
         
         machine.calculating(babyZay);
         machine.calculating(babyMarwan);

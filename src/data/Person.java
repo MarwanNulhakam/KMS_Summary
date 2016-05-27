a0d104cdb2ee02a01a173fdf3280c1ba699c44ba
MarwanNulhakam <wind_raider_zero@yahoo.com>
2016-05-27 14:08:34 +0800

    MN | change constructor's passed parameter which using age into birth of date and age automatically calculated by compare birth of date and current measurement date

--- src/data/Person.java
+++ src/data/Person.java
@@ -14,6 +14,7 @@ public class Person {
     private String name;
     private boolean isMale;
     private int age;
+    private String dateOfBirth;
     private double weight;
     private double previousWeight;
     private double secondLastWeight;
@@ -27,23 +28,30 @@ public class Person {
     public boolean Tidak2Kali;
     
     //constructor
-    public Person(  String name, boolean isMale, int age, double weight, 
+    public Person(  String name, boolean isMale, String dateOfBirth,double weight, 
                     double previousWeight,String lastVisitDate, double secondLastWeight,
                     String secondLastVisitDate){
         this.name=name;
         this.isMale=isMale;
-        this.age=age;
+        this.dateOfBirth=dateOfBirth;
+        this.age=monthAges(dateOfBirth,new java.text.SimpleDateFormat("yyyy/MM/dd").format(new java.util.Date()));
         this.weight=weight;
         this.previousWeight=previousWeight;
         this.lastVisitDate=lastVisitDate;
         this.secondLastWeight=secondLastWeight;
         this.secondLastVisitDate=secondLastVisitDate;
     }
-    
+    private int monthAges(String lastVisitDate,String currentDate){
+        int tahun = Integer.parseInt(currentDate.substring(0,4))-Integer.parseInt(lastVisitDate.substring(0,4));
+        int bulan = Integer.parseInt(currentDate.substring(5,7))-Integer.parseInt(lastVisitDate.substring(5,7));
+        int hari = Integer.parseInt(currentDate.substring(8))-Integer.parseInt(lastVisitDate.substring(8));
+        return(tahun*12 + bulan + (int)(hari/30));
+    }
     //mutators
     public String getName(){return name;}
     public boolean isMale(){return isMale;}
     public int getAge(){return age;}
+    public String getDateOfBirth(){return dateOfBirth;}
     public double getWeight(){return weight;}
     public double getPreviousWeight(){return previousWeight;}
     public String getLastVisitDate(){return lastVisitDate;}
